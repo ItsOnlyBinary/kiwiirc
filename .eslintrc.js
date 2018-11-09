5db5ce6b3d4df180844c00f4d09211e80c417423
ItsOnlyBinary <github@itsonlybinary.com>
2018-11-09 15:18:15 +0000

    Add Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,9 @@ module.exports = {
     },
     extends: [
         'airbnb-base',
+        'plugin:prettier/recommended',
+        'prettier/standard',
         'plugin:vue/recommended',
-        'standard'
     ],
     env: {
         'browser': true,
@@ -15,22 +16,16 @@ module.exports = {
     // required to lint *.vue files
     plugins: [
         'vue',
+        'prettier',
     ],
     // add your custom rules here
     'rules': {
+        "prettier/prettier": "error",
         'class-methods-use-this': 0,
-        'comma-dangle': ['error', {
-            'arrays': 'always-multiline',
-            'objects': 'always-multiline',
-            'imports': 'never',
-            'exports': 'never',
-            'functions': 'ignore'
-        }],
         'import/extensions': 0,
         'import/no-extraneous-dependencies': 0,
         'import/no-unresolved': 0,
         'import/prefer-default-export': 0,
-        'indent': ['error', 4],
         'no-continue': 0,
         'no-multi-assign': 0,
         'no-param-reassign': ['error', { 'props': false }],
@@ -43,8 +38,6 @@ module.exports = {
         'prefer-const': 0,
         'prefer-destructuring': 0,
         'prefer-template': 0,
-        'semi': ['error', 'always'],
-        'space-before-function-paren': ['error', 'never'],
         'vue/html-indent': ['error', 4],
         'vue/max-attributes-per-line': 0,
         'vue/require-prop-types': 0,

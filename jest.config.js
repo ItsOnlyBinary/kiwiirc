d579cdcd62b613057b580b9696e0c3c5e517c13c
ItsOnlyBinary <git@itsonlybinary.com>
2023-07-18 16:51:26 +0100

    New build system

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,26 @@
 module.exports = {
-    preset: '@vue/cli-plugin-unit-jest',
     collectCoverage: true,
-    collectCoverageFrom: ['/src/**/*.{js,jsx,vue}'],
+    collectCoverageFrom: ['src/**/*.{js,jsx,vue}'],
+    coverageProvider: 'babel',
     coverageDirectory: 'tests/coverage/',
     coverageReporters: ['html', 'text-summary'],
+    testEnvironment: 'jsdom',
+    moduleFileExtensions: [
+        'js',
+        'json',
+        'vue',
+    ],
+    transform: {
+        '^.+\\.vue$': require.resolve('@vue/vue3-jest'),
+        '^.+\\.jsx?$': require.resolve('babel-jest'),
+        '.+\\.(css|less|sass|scss|png|jpg|svg|ttf|woff|woff2)$': 'jest-transform-stub',
+    },
+    transformIgnorePatterns: ['/node_modules/'],
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+    },
+    testMatch: [
+        '**/tests/unit/**/*.spec.[jt]s?(x)',
+        '**/__tests__/*.[jt]s?(x)',
+    ],
 };

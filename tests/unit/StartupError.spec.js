3b7ce18b64e446847e6915b0b801222fb78be62d
ItsOnlyBinary <git@itsonlybinary.com>
2024-11-24 14:06:25 +0000

    Fix tests

--- tests/unit/StartupError.spec.js
+++ tests/unit/StartupError.spec.js
@@ -1,20 +1,24 @@
-import Vue from 'vue';
+import { createApp } from 'vue';
 import StartupError from '@/components/StartupError';
-
-Vue.mixin({
-    computed: {
-        $t() {
-            return (key, options) => key;
-        },
-    },
-});
 
 describe('StartupError.vue', () => {
     it('should render correct contents', () => {
-        const vm = new Vue({
-            el: document.createElement('div'),
-            render: (h) => h(StartupError, { props: { error: 'some error' } }),
+        const app = createApp(
+            StartupError,
+            { error: 'some error' }
+        );
+
+        app.mixin({
+            computed: {
+                $t() {
+                    return (key, options) => key;
+                },
+            },
         });
-        expect(vm.$el.querySelector('span.kiwi-error-text').textContent).toEqual('some error');
+
+        const el = document.createElement('div');
+        app.mount(el);
+
+        expect(el.querySelector('span.kiwi-error-text').textContent).toEqual('some error');
     });
 });

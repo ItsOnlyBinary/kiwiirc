93ea2545a8befad2ddd8066ae9e0ae8de10ae352
ItsOnlyBinary <git@itsonlybinary.com>
2025-09-15 12:37:15 +0100

    wip

--- build/vite/pluggable-exports.js
+++ build/vite/pluggable-exports.js
@@ -23,59 +23,38 @@ export default function pluggableExportsPlugin() {
         },
 
 
-        async transform(code, id) {
-            if (!filter(id)) return;
+        // async transform(code, id) {
+        //     if (pluginDisabled || !filter(id)) return;
 
-            if (!code.includes(`void 'kiwi public'`)) {
-                console.log('skipping', id);
-                return;
-            }
+        //     if (!code.includes(`void 'kiwi public'`)) {
+        //         console.log('skipping', id);
+        //         return;
+        //     }
 
-            console.log('transforming', id);
+        //     console.log('transforming', id);
 
-            // try {
-            //     const ast = babel.parseSync(code, {
-            //         sourceType: 'module',
-            //         plugins: [],
-            //     });
+        //     try {
+        //         const ast = babel.parseSync(code, {
+        //             sourceType: 'module',
+        //             plugins: [],
+        //         });
 
-            //     console.log('code', code);
-            //     console.log('ast', ast.program.body[0]);
+        //         // console.log('code', code);
 
-            //     traverse.default(ast, {
-            //         Program: {
-            //             exit() {
-            //                 process.exit(0);
-            //             },
-            //         },
-            //         StringLiteral(path) {
-            //             const value = path.node.value;
-            //             if (value === void 'kiwi public') {
-            //                 console.log('removing');
-            //                 path.remove();
-            //             } else {
-            //                 console.log('expr', value);
-            //             }
-            //         },
-            //         ExpressionStatement(path) {
-            //             const expr = path.node.expression;
-            //             if (
-            //                 path.parent.type === 'Program' &&
-            //                 expr.type === 'StringLiteral' &&
-            //                 expr.value === void 'kiwi public'
-            //             ) {
-            //                 console.log('Matched top-level string literal:', expr.value);
-            //                 console.log('Location:', expr.loc?.start);
-            //             }
-            //         }
-            //     });
+        //         traverse.default(ast, {
+        //             Program: {
+        //                 exit(path) {
 
-            //     const output = generate.default(ast, { sourceMaps: true, sourceFileName: id }, code);
-            //     return { code: output.code, map: output.map };
+        //                 },
+        //             },
+        //         });
 
-            // } catch (error) {
-            //     console.log('error', id, error);
-            // }
-        },
+        //         const output = generate.default(ast, { sourceMaps: true, sourceFileName: id }, code);
+        //         return { code: output.code, map: output.map };
+
+        //     } catch (error) {
+        //         console.log('error', id, error);
+        //     }
+        // },
     }
 }
